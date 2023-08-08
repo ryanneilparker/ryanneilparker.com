bc419d14084801c0b76dbd3fd1eaa80b6fcf5217
= <ryanneilparker@gmail.com>
2023-08-08 23:21:02 +0200

    Add basic web pages

---
+++ RyanNeilParker/Pages/Experiments.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RyanNeilParker.Pages
+{
+    public class ExperimentsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ RyanNeilParker/Pages/Thoughts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RyanNeilParker.Pages
+{
+    public class ThoughtsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
